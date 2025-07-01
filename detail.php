12fcb85f5425fd6a9b89aea1216d04484adff2ff
thomias <celia.thomias@epitech.eu>
2025-07-01 18:30:20 +0200

    Amélioration du responsive : ajustement des ellipses sur mobile en mode clair et sombre, optimisation des tailles et positions

--- detail.php
+++ detail.php
@@ -11,6 +11,7 @@ $projet = [
 <html lang="fr">
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title><?= $projet['titre'] ?></title>
   <link rel="stylesheet" href="style.css">
 </head>
